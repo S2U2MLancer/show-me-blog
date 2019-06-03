1cf7addebc776bc8399f0274dbd29876493fec1b
Yangyang Xia <Yangyang.Xia@ygomi.com>
2019-06-03 18:18:26 +0800

    add redux code

--- web/src/app.jsx
+++ web/src/app.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
+import Body from './components/body';
 
-function App() {
-  return (
-    <div className="container">
-      <h1>Hello World React!!!</h1>
-    </div>
-  );
-}
+const state = {
+  getIndexInfoReducers: {
+    indexInfo: {
+      content: 'Hello',
+    },
+  },
+};
+const store = createStore(rootReducer, state);
+
+const App = () => (
+  <div className="container">
+    <Body />
+  </div>
+);
+
 
 const app = document.createElement('div');
 document.body.appendChild(app);
-ReactDOM.render(<App />, app);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  app,
+);
