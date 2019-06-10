ff1af6328327e53ce86334b526c1d2a75752dfdf
Yangyang Xia <Yangyang.Xia@ygomi.com>
2019-06-10 09:02:51 +0800

    set up basic server

---
+++ server/src/main/java/com/s2u2m/prod/showme/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.s2u2m.prod.showme.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/test/{ping}")
+    public String pingPong(@PathVariable String ping) {
+        return ping;
+    }
+}
