833a68e676138321c68608e255938dee115b35eb
Yangyang Xia <Yangyang.Xia@ygomi.com>
2019-06-28 08:57:49 +0800

    add test for template

---
+++ web/src/pages/template/index.test.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Template from '.';
+
+const testElement = () => (
+  <div />
+);
+
+function setup() {
+  const props = {
+    children: testElement,
+  };
+
+  const enzymeWrapper = shallow(<Template {...props} />);
+
+  return {
+    props,
+    enzymeWrapper,
+  };
+}
+
+describe('index componet', () => {
+  it('show', () => {
+    const { enzymeWrapper } = setup();
+    expect(enzymeWrapper.find('div.col-md-3').text())
+      .toContain('Left Navigator');
+  });
+});
