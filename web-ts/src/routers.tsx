76e8729e0228d62f8914c80b56f569915b5dcff7
Yangyang Xia <Yangyang.Xia@ygomi.com>
2019-07-19 13:23:13 +0800

    add template

--- web-ts/src/routers.tsx
+++ web-ts/src/routers.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from './pages/app';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import App from './pages/App';
 
 const Routers = () => (
   <Router>
-    <Route path="/" component={App} />
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          Left Navigator
+        </div>
+        <div className="col-md-8">
+          <Switch>
+            <Route path="/" exact={true} component={App} />
+          </Switch>
+        </div>
+      </div>
+    </div>
   </Router>
 );
 
