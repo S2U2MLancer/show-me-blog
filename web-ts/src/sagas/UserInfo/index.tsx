43ac59eca3cd4767cfdb65cbf3310159807ae54c
Amos Xia <xiayy860612@126.com>
2019-07-29 07:28:05 +0800

    get data from server

--- web-ts/src/sagas/UserInfo/index.tsx
+++ web-ts/src/sagas/UserInfo/index.tsx
@@ -1,16 +1,16 @@
 import { call, put, takeEvery, CallEffect } from 'redux-saga/effects';
 import ActionEvent from '../../actions/ActionEvent';
 import AppActionProducer from '../../actions';
-import axios from 'axios';
+import axios, {AxiosResponse} from 'axios';
 import { UserInfo } from '../../reducers/UserInfo';
 
 function* requestReceiveUserInfo() {
-  const response = yield call(() => axios.get("/static/userInfo.json"));
+  const response: AxiosResponse<UserInfo> = yield call(
+      () => axios.get<UserInfo>("/static/userInfo.json"));
   if (response && response.status === 200) {
-    const userInfo: UserInfo = response.data;
     yield put(AppActionProducer.UserActions.storeUserInfo({
       id: 'id',
-      userInfo
+      userInfo: response.data
     }))
   }
 }
